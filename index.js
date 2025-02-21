a45b6ac52bd3de5a1a85a31d0f8498a42bc45d27
thanhzanh <danhthanhvo23072003@gmail.com>
2025-02-21 15:51:24 +0700

    Tinh nang dang ky tai khoan

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const database = require('./config/database');
 const routeApiVer1 = require("./api/v1/routes/index.route");
@@ -11,6 +13,12 @@ const port = process.env.PORT;
 
 // parse application/json
 app.use(bodyParser.json());
+
+// cors
+app.use(cors());
+
+// cookie
+app.use(cookieParser());
 
 routeApiVer1(app);
 
